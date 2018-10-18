d88e9a2ef8daadbfafbd2986e970de5d88d00326
RodChong <rodchong@outlook.com>
2018-10-18 21:43:42 +0800

    删除查看班级

--- studentphoto/classinfo.py
+++
@@ -1,8 +0,0 @@
-import re
-import requests
-
-resp = requests.get('http://jwzx.cqu.pt/kebiao/index.php')
-resp = resp.text
-result = re.findall(
-    r'(<div id="kbTabs-bj".+)<div id="kbTabs-kc"', resp, re.RegexFlag.DOTALL)
-print(result[0])
