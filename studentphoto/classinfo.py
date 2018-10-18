b8b0db7af0c449c273c8ac70b4dbd8a632af1100
RodChong <rodchong@outlook.com>
2018-10-18 21:13:26 +0800

    修改正则表达式

--- studentphoto/classinfo.py
+++ studentphoto/classinfo.py
@@ -2,6 +2,7 @@ import re
 import requests
 
 resp = requests.get('http://jwzx.cqu.pt/kebiao/index.php')
-resp = resp.text.replace('\n','').replace('\r','')
-result = re.findall(r'(<div id="kbTabs-bj".+)<div id="kbTabs-kc"', resp)
+resp = resp.text
+result = re.findall(
+    r'(<div id="kbTabs-bj".+)<div id="kbTabs-kc"', resp, re.RegexFlag.DOTALL)
 print(result[0])
