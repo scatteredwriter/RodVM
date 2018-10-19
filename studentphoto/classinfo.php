6546df6374f59bbde2e2a905e8e60c8b64a12c55
RodChong <rodchong@outlook.com>
2018-10-19 12:42:32 +0000

    完善班级信息

--- studentphoto/classinfo.php
+++ studentphoto/classinfo.php
@@ -16,14 +16,17 @@
 <body>
     <?php
 $ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,"http://www.devdo.net");
+curl_setopt($ch,CURLOPT_URL,'http://jwzx.cqu.pt/kebiao/index.php');
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch,CURLOPT_HEADER,0);
 curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
 $output = curl_exec($ch);
 curl_close($ch);
-preg_match('<div id="kbTabs-bj".+(?=<div id="kbTabs-kc")', $output, $result);
-echo $result;
+preg_match('/<div id="kbTabs-bj".+(?=<div id="kbTabs-kc")/s', $output, $result);
+$output = $result[0];
+$output = preg_replace('/kb_bj\.php\?bj=(\d+)/','javascript:showclassinfo($1);',$output);
+echo $output;
 ?>
 
 </body>
